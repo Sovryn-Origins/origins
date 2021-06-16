a183a81a9c6e1895ab653958fe20f9cece0f070c
Franklin Richards <powerhousefrank@protonmail.com>
2021-06-16 14:27:53 +0530

    ERC20Detailed now uses ERC20 rather than IERC20_

--- contracts/Openzeppelin/ERC20Detailed.sol
+++ contracts/Openzeppelin/ERC20Detailed.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
 
-import "./IERC20_.sol";
+import "./ERC20.sol";
 
 /**
  * @dev Optional functions from the ERC20 standard.
  */
-contract ERC20Detailed is IERC20_ {
+contract ERC20Detailed is ERC20 {
 	string private _name;
 	string private _symbol;
 	uint8 private _decimals;
